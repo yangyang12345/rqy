726997b326b6a064f5503ef413c2b30f7963c3d3
yangyang12345 <13986670515@163.com>
2019-06-23 14:44:06 +0800

    bug调整

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -157,7 +157,7 @@ class CheckController extends Controller{
 
         $now = DB::table('task_record as t')
             ->leftJoin('shop as s', 't.shop_id', '=', 's.id')
-            ->select('t.id', 't.user_id', 't.wrap_type', 't.task_type', 't.goods_key', 's.name', 't.goods_url', 't.total')
+            ->select('t.id', 't.user_id', 't.wrap_type', 't.task_type', 't.goods_key', 's.store_name', 't.goods_url', 't.total')
             ->where('id', '=', $id)
             ->first();
 
